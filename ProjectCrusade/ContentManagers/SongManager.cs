6dcebf870e3e01da4cef7b0296e72f61439c4e12
Christian Cosgrove <christian.cosgrove@gmail.com>
2016-03-12 13:46:45 -0500

    Manager reorganization, add SongManager and SoundEffectManager classes

---
+++ ProjectCrusade/ContentManagers/SongManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+
+namespace ProjectCrusade
+{
+	public class SongManager
+	{
+
+		Dictionary<string, Song> songs;
+		public SongManager (ContentManager content)
+		{
+			songs = new Dictionary<string, Song> ();
+
+			//Automatically load all files in Fonts folder.
+
+			DirectoryInfo dir = new DirectoryInfo (content.RootDirectory + "/Songs/");
+
+			FileInfo[] files = dir.GetFiles ("*.*");
+			foreach (FileInfo file in files) {
+				string key = Path.GetFileNameWithoutExtension (file.Name);
+				songs [key] = content.Load<Song> ("Songs/" + key);
+			}
+		}
+
+		public Song GetSong(string id) { return songs[id]; }
+	}
+}
+
---
+++ ProjectCrusade/ContentManagers/SoundEffectManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+
+namespace ProjectCrusade
+{
+	public class SoundEffectManager
+	{
+
+		Dictionary<string, SoundEffect> soundEffects;
+		public SoundEffectManager (ContentManager content)
+		{
+			soundEffects = new Dictionary<string, SoundEffect> ();
+
+			//Automatically load all files in Fonts folder.
+
+			DirectoryInfo dir = new DirectoryInfo (content.RootDirectory + "/SoundEffects/");
+
+			FileInfo[] files = dir.GetFiles ("*.*");
+			foreach (FileInfo file in files) {
+				string key = Path.GetFileNameWithoutExtension (file.Name);
+				soundEffects [key] = content.Load<SoundEffect> ("SoundEffects/" + key);
+			}
+		}
+
+		public SoundEffect GetSoundEffect(string id) { return soundEffects[id]; }
+	}
+}
+
