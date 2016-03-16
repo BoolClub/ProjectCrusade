814d781bd01156332db7509a99182719c35eda4f
Christian Cosgrove <christian.cosgrove@gmail.com>
2016-03-16 19:40:07 -0400

    Standardized enemy move speeds
    Move speeds are now in pixels/sec.

--- ProjectCrusade/Entity/Enemy.cs
+++ ProjectCrusade/Entity/Enemy.cs
@@ -8,20 +8,25 @@ namespace ProjectCrusade
 	{
 		public float Health { get; set; }
 
-
+		/// <summary>
+		/// How many pixels/sec the enemy moves.
+		/// </summary>
+		public float Speed { get; protected set; }
 
 		public Enemy ()
 		{
 			Width = 16;
 			Height = 16;
+			Speed = 120;
 		}
 
 		public override void Update (GameTime gameTime, World world) 
 		{
 			const float interactionRadius = 1000;
 			if ((world.Player.Position - Position).LengthSquared () < interactionRadius * interactionRadius) {
+				float displacement = (float)gameTime.ElapsedGameTime.TotalSeconds * Speed;
 				Vector2 nvec = Vector2.Normalize (world.Player.Position - Position);
-				Position += nvec;
+				Position += displacement * nvec;
 			}
 		}
 
